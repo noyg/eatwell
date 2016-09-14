65d5640003bd25bb154750e44b47bc2c819831dd
Noy <noy.gabay@wework.com>
2016-09-14 15:07:01 +0300

    Stop fussing around and let devise_token_auth have all the metadata it needs

--- db/migrate/20160912145235_devise_token_auth_create_users.rb
+++ db/migrate/20160912145235_devise_token_auth_create_users.rb
@@ -2,13 +2,13 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.0]
   def change
     change_table(:users) do |t|
       ## Required
-      # t.string :provider, :null => false, :default => 'email'
-      # t.string :uid, :null => false, :default => ''
+      t.string :provider, :null => false, :default => 'email'
+      t.string :uid, :null => false, :default => ''
 
       ## Tokens
       t.json :tokens
     end
 
-    # add_index :users, [:uid, :provider], :unique => true
+    add_index :users, [:uid, :provider], :unique => true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -26,12 +26,12 @@ ActiveRecord::Schema.define(version: 20160912145235) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",      null: false
+    t.string   "encrypted_password",     default: "",      null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,       null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -40,15 +40,18 @@ ActiveRecord::Schema.define(version: 20160912145235) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,  null: false
+    t.integer  "failed_attempts",        default: 0,       null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.string   "provider",               default: "email", null: false
+    t.string   "uid",                    default: "",      null: false
     t.json     "tokens"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
   end
 
